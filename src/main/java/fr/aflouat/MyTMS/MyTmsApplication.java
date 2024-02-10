0f45193a9e9f38e294201e4c21bbd23bc0490427
aflouat <aflouat@gmail.com>
2024-02-10 23:22:13 +0100

    first commit

---
+++ src/main/java/fr/aflouat/MyTMS/MyTmsApplication.java
@@ -0,0 +1,13 @@
+package fr.aflouat.MyTMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTmsApplication.class, args);
+	}
+
+}
